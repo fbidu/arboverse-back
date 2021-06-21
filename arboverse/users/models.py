5db02ca5a351348a5d8d4f3f2b0c45d2c221d95e
Osvaldo Reis Junior <osvaldoreisss@gmail.com>
2021-06-21 18:02:02 -0300

    Model and test for virus

--- arboverse/users/models.py
+++ arboverse/users/models.py
@@ -16,3 +16,28 @@ class User(AbstractUser):
         return reverse(
             "users:detail", kwargs={"username": self.username}
         )
+
+class Virus(models.Model):
+    virus_name = models.TextField(default='')
+    specie = models.TextField(default='')
+    family = models.TextField(default='')
+    abbreviation = models.TextField(default='')
+    collection_date = models.DateField()
+    genome_type = models.TextField(default='')
+    enveloped = models.BooleanField()
+    reference_strain = models.TextField(default='')
+    genome_length_nt = models.IntegerField(default='')
+    borning = models.TextField(default='')
+    host_amplifier = models.TextField(default='')
+    human_fatal_disease = models.BooleanField(default='')
+    veterinary_diseases = models.BooleanField(default='')
+    veterinary_fatal_diseases = models.BooleanField(default='')
+    no_cases = models.TextField(default='')
+    level_of_disease = models.TextField(default='')
+    vaccine = models.TextField(default='')
+    vero_cells = models.BooleanField(default='')
+    C6_36_cells = models.BooleanField(default='')
+    cpe_vero = models.TextField(default='')
+    plaques_vero = models.TextField(default='')
+    animal_model = models.TextField(default='')
+    sals_level = models.TextField(default='')
