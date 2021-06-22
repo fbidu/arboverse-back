f1c37815a740dc5cd49d4e8b31f7141a8deb07a0
Osvaldo Reis Junior <osvaldoreisss@gmail.com>
2021-06-22 09:58:23 -0300

    Create new app arbovirus and moved virus model and tests from user app to arbovirus app

---
+++ arboverse/arbovirus/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Virus(models.Model):
+    virus_name = models.TextField(default='')
+    specie = models.TextField(default='')
+    family = models.TextField(default='')
+    abbreviation = models.TextField(default='')
+    collection_date = models.TextField(default='')
+    genome_type = models.TextField(default='')
+    enveloped = models.BooleanField()
+    reference_strain = models.TextField(default='')
+    genome_length_nt = models.IntegerField(default='')
+    borning = models.TextField(default='')
+    host_amplifier = models.TextField(default='')
+    human_fatal_disease = models.BooleanField(default='')
+    veterinary_diseases = models.BooleanField(default='')
+    veterinary_fatal_diseases = models.BooleanField(default='')
+    no_cases = models.TextField(default='')
+    level_of_disease = models.TextField(default='')
+    vaccine = models.TextField(default='')
+    vero_cells = models.BooleanField(default='')
+    C6_36_cells = models.BooleanField(default='')
+    cpe_vero = models.TextField(default='')
+    plaques_vero = models.TextField(default='')
+    animal_model = models.TextField(default='')
+    sals_level = models.TextField(default='')
--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "arboverse.users.apps.UsersConfig",
+    "arboverse.arbovirus.apps.ArbovirusConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
