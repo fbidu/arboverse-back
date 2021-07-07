d48bd7d28057365a69ac7984f028476a579e0f49
Jacqueline Atsuko Tida <jacquelinetida@gmail.com>
2021-07-07 13:24:08 -0500

    add tree cover loss by dominant drivers
    adjust fill type for this layer, and applied color style.

--- arboverse/static/js/mapboxutil.js
+++ arboverse/static/js/mapboxutil.js
@@ -1,6 +1,6 @@
 // TO MAKE THE MAP APPEAR YOU MUST ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
-mapboxgl.accessToken = '<your access token here>';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJib3ZlcnNlIiwiYSI6ImNrbXA2ODdnMzJibDAycXF1ODc2dmJtNngifQ.qHL3R2dqFpECCzUckaSl3w';
 
 // initiate a new map by passing an object describing a config
 // for more info see: https://docs.mapbox.com/mapbox-gl-js/api/map/
@@ -24,8 +24,8 @@ var map = new mapboxgl.Map({
 //   (mapVar) the Mapbox map object
 //   (title) a string identifier for the source and layer
 //   (url) the raster tile URL to add to the map
-const addTileLayerToMap = (mapVar, title, url, type, source_layer) => {
-    console.log(mapVar, title, url  )
+const addTileLayerToMap = (mapVar, title, url, type, paint, source_layer) => {
+    console.log(mapVar, title, url)
     // need to first add a source
     mapVar.addSource(title, {
         type: 'vector',
@@ -39,6 +39,7 @@ const addTileLayerToMap = (mapVar, title, url, type, source_layer) => {
         'id': title,
         'type': type,
         'source': title,
+        'paint': paint,
         "source-layer": source_layer
     });
 
@@ -48,8 +49,6 @@ const addTileLayerToMap = (mapVar, title, url, type, source_layer) => {
         'none'
     );
 }
-
-
 // ...
 // var map = new mapboxgl.Map({
 // ...
@@ -58,7 +57,7 @@ function update_map(cb) {
     var clickedLayer = cb.id
     console.log(cb.id)
     console.log(map.getStyle().layers)
-    if (cb.checked){
+    if (cb.checked) {
         map.setLayoutProperty(
             clickedLayer,
             'visibility',
@@ -79,6 +78,6 @@ function update_map(cb) {
 
 // run the API call once the map is loaded (API call is asnyc)
 map.on('load', async () => {
-        // add a layer to the map
-    addTileLayerToMap(map, 'arboverse.bkdd701g', 'mapbox://arboverse.bkdd701g', 'line', 'Cover_loss_dominant_drivers-bbybfd');
+    // add a layer to the map
+    addTileLayerToMap(map, 'arboverse.bkdd701g', 'mapbox://arboverse.bkdd701g', 'fill', { 'fill-color': ['match', ['get', 'classes'], [1], "#f94144", [2], "#f9844a", [3], "#43aa8b", [4], "#f9c74f", [5], "#6930c3", "#000000"] }, 'Cover_loss_dominant_drivers-bbybfd');
 });
