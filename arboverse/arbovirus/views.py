8b4a3aea90539f3f6dcffafb46c03c07ba379499
Osvaldo Reis Junior <osvaldoreisss@gmail.com>
2021-08-25 17:10:22 -0300

    Change route of arboverse to be created with arbovirus.views

--- arboverse/arbovirus/views.py
+++ arboverse/arbovirus/views.py
@@ -1,1 +1,23 @@
 # Create your views here.
+from django.shortcuts import render,get_object_or_404
+
+from arboverse.arbovirus.models import  Virus
+
+from django.db.models import Q
+
+def search(request):
+
+    results = []
+
+    if request.method == "GET":
+
+        query = request.GET.get('search')
+
+        if query == '':
+
+            query = 'Abadina'
+        query = 'Abadina'
+        print(query)
+        results = Virus.objects.filter(name__icontains=query)
+
+    return render(request, 'pages/arboverse.html', {'results': results})
