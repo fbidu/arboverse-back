2162b041cd325129bfa2a0aaaceca277749df3c0
Noah Perry <perrynoahchristian@gmail.com>
2024-12-02 13:54:06 -0500

    vectors set up correctly now

--- arboverse_updated/serializers.py
+++ arboverse_updated/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Virus, VirusVector, VectorSpecies
+from .models import VectorSpecies, FeedingPeriod, BloodMeal, Landscape, Habitat, Location, VectorGenus, Virus, VirusVector
 
 
 class VirusSerializer(serializers.ModelSerializer):
@@ -20,7 +20,50 @@ class VectorSpeciesSerializer(serializers.ModelSerializer):
         fields = ['name']
 
 
+class FeedingPeriodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FeedingPeriod
+        fields = '__all__'
+
+class BloodMealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BloodMeal
+        fields = '__all__'
+
+class LandscapeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Landscape
+        fields = '__all__'
+
+class HabitatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habitat
+        fields = '__all__'
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+class VectorGenusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VectorGenus
+        fields = '__all__'
+
+class VirusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Virus
+        fields = '__all__'
+
 class VectorSpeciesAllSerializer(serializers.ModelSerializer):
+    feeding_period = FeedingPeriodSerializer(many=True, read_only=True)
+    blood_meal = BloodMealSerializer(many=True, read_only=True)
+    landscape = LandscapeSerializer(many=True, read_only=True)
+    habitat = HabitatSerializer(many=True, read_only=True)
+    location = LocationSerializer(many=True, read_only=True)
+    genus = VectorGenusSerializer(read_only=True)
+    virus = VirusSerializer(many=True, read_only=True)
+
     class Meta:
         model = VectorSpecies
         fields = '__all__'
